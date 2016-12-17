b6c287c98227971b982a85b4215e4868fd1cc087
motoki-shun <motoki-shun@kayac.com>
2016-12-17 23:26:16 +0900

    だいたい出来た

--- _src/js/entry.js
+++ _src/js/entry.js
@@ -1,9 +1,62 @@
-// import $ from 'jquery';
-// import { EventEmitter2 } from 'eventemitter2';
-// import Util from './modules/Util';
+import FileSelector from './modules/FileSelector';
 
-// export default class Main extends EventEmitter2 {
-//   constructor(opts = {}) {
-//     super();
-//   }
-// }
+const MAX_FILE_SIZE = 15 * 1000 * 1000; // (byte), 5MB
+
+const input = document.querySelector('.js-input-image');
+const preview = document.querySelector('.js-preview');
+const canvas = document.querySelector('.js-canvas');
+const ctx = canvas.getContext('2d');
+
+const fileSelector = new FileSelector({
+    input: input,
+});
+
+fileSelector.on('select_file', files => {
+    const file = files[0];
+    if (!files || !files.length) {
+        fileSelector.resetFiles();
+        return;
+    }
+    if (!file.type || !file.type.match('image.*')) {
+        fileSelector.resetFiles();
+        return;
+    }
+    if (file.size > MAX_FILE_SIZE) {
+        fileSelector.resetFiles();
+        return;
+    }
+});
+
+fileSelector.on('load_img', image => {
+    const size = Math.max(image.width, image.height);
+    setCanvasSize(size, size);
+    draw(image, size);
+    appendImg(canvas.toDataURL());
+});
+
+function appendImg(src) {
+    const img = new Image();
+    img.src = src;
+    preview.appendChild(img);
+}
+
+function draw(image, size) {
+    const width = image.width || size;
+    const height = image.height || size;
+    const x = (size - width) / 2;
+    const y = (size - height) / 2;
+    ctx.save();
+    ctx.fillStyle = '#fff';
+    ctx.fillRect(0, 0, size, size);
+    ctx.beginPath();
+    ctx.drawImage(image, x, y, width, height);
+    ctx.restore();
+}
+
+function setCanvasSize(width, height) {
+    canvas.style.width = width + "px";
+    canvas.style.height = height + "px";
+    canvas.width = width * 2; // for retina
+    canvas.height = height * 2; // for retina
+    ctx.scale(2, 2); // for retina
+}
---
+++ _src/js/modules/FileSelector.js
@@ -0,0 +1,57 @@
+import EventEmitter from 'events';
+import loadImage from 'blueimp-load-image';
+import { parseMetaData } from 'blueimp-load-image';
+
+export default class FileSelector extends EventEmitter {
+  constructor(opts = {}) {
+    super();
+
+    if (!this.isSupport()) {
+      return false;
+    }
+
+    this.input = opts.input || document.querySelector('input[type=file]');
+
+    if (!this.input) {
+      return;
+    }
+
+    this.input.addEventListener('change', this._handleFileSelect.bind(this), false);
+  }
+
+  _handleFileSelect(e) {
+    const files = e.target.files;
+    if (files.length <= 0) {
+      // ファイルがないときは無視
+      return;
+    }
+    for (let i = 0, len = files.length; i < len; i++) {
+      const file = files[i];
+      if (file.type && !file.type.match('image.*')) {
+        // file.typeが入っていない場合はスキップ(Android対策)
+        continue;
+      }
+      this._handleFileReader(file);
+    }
+    this.emit('select_file', files);
+  }
+
+  _handleFileReader(file) {
+    parseMetaData(file, data => {
+      const orientation = data.exif ? parseInt(data.exif.get('Orientation'), 10) : 0;
+      loadImage(file, image => {
+        this.emit('load_img', image);
+      }, {
+        orientation,
+      });
+    });
+  }
+
+  isSupport() {
+    return (window.File != null) && (window.FileReader != null) && (window.FileList != null) && (window.Blob != null);
+  }
+
+  resetFiles() {
+    this.input.value = '';
+  }
+}
