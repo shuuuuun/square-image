d2b0b29dfe306b0f748879f48b7cb066a6733a18
motoki-shun <motoki-shun@kayac.com>
2016-12-18 12:27:41 +0900

    サイズと背景を変更できるように

--- _src/js/entry.js
+++ _src/js/entry.js
@@ -2,8 +2,10 @@ import FileSelector from './modules/FileSelector';
 
 const MAX_FILE_SIZE = 15 * 1000 * 1000; // (byte), 5MB
 const MAX_CANVAS_SIZE = 2000; // 大きすぎるとiosなどで動かない
-const BG_COLOR = '#fff';
+const BG_COLOR = '#ffffff';
 
+const sizeInput = document.querySelector('.js-input-size');
+const colorInput = document.querySelector('.js-input-color');
 const input = document.querySelector('.js-input-image');
 const preview = document.querySelector('.js-preview');
 const canvas = document.querySelector('.js-canvas');
@@ -12,6 +14,9 @@ const ctx = canvas.getContext('2d');
 const fileSelector = new FileSelector({
     input: input,
 });
+
+sizeInput.value = MAX_CANVAS_SIZE;
+colorInput.value = BG_COLOR;
 
 fileSelector.on('select_file', files => {
     const file = files[0];
@@ -30,7 +35,8 @@ fileSelector.on('select_file', files => {
 });
 
 fileSelector.on('load_img', image => {
-    const size = Math.min(Math.max(image.width, image.height), MAX_CANVAS_SIZE);
+    const maxSize = sizeInput.value || MAX_CANVAS_SIZE;
+    const size = Math.min(Math.max(image.width, image.height), maxSize);
     console.log(image.width, image.height, size);
     setCanvasSize(size, size);
     draw(image, size);
@@ -44,6 +50,7 @@ function appendImg(src) {
 }
 
 function draw(image, size) {
+    const bgColor = colorInput.value || BG_COLOR;
     const aspect = image.width / image.height;
     let width = image.width || size;
     let height = image.height || size;
@@ -59,7 +66,7 @@ function draw(image, size) {
     const x = (size - width) / 2;
     const y = (size - height) / 2;
     ctx.save();
-    ctx.fillStyle = BG_COLOR;
+    ctx.fillStyle = bgColor;
     ctx.fillRect(0, 0, size, size);
     ctx.beginPath();
     ctx.drawImage(image, x, y, width, height);
