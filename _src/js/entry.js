c5d2eba2f0a6c84d6408b67823fd909f2b7ac27a
motoki-shun <motoki-shun@kayac.com>
2018-07-01 15:50:41 +0900

    比率を変えられるようにする！

--- _src/js/entry.js
+++ _src/js/entry.js
@@ -1,12 +1,14 @@
 import FileSelector from './modules/FileSelector';
 
-const MAX_CANVAS_SIZE = 1080; // (px), 大きすぎるとiosなどで動かない
+const MAX_CANVAS_SIZE = 1500; // (px), 大きすぎるとiosなどで動かない
 const PADDING = 0; // (px)
 const BG_COLOR = '#ffffff';
+const RATIO = 0.8;
 
 const sizeInput = document.querySelector('.js-input-size');
 const paddingInput = document.querySelector('.js-input-padding');
 const colorInput = document.querySelector('.js-input-color');
+const ratioInput = document.querySelector('.js-input-ratio');
 const input = document.querySelector('.js-input-image');
 const preview = document.querySelector('.js-preview');
 const canvas = document.querySelector('.js-canvas');
@@ -19,13 +21,14 @@ const fileSelector = new FileSelector({
 sizeInput.value = MAX_CANVAS_SIZE;
 paddingInput.value = PADDING;
 colorInput.value = BG_COLOR;
+ratioInput.value = RATIO;
 
 fileSelector.on('load_img', (image, file) => {
     const maxSize = sizeInput.value || MAX_CANVAS_SIZE;
     const size = Math.min(Math.max(image.width, image.height), maxSize);
-    console.log(file);
+    // console.log(file);
     console.log(image.width, image.height, size);
-    setCanvasSize(size, size);
+    // setCanvasSize(size, size);
     draw(image, size);
     appendImg(canvas.toDataURL(), file.name);
 });
@@ -41,6 +44,7 @@ function appendImg(src, alt) {
 function draw(image, size) {
     const padding = paddingInput.value || PADDING;
     const bgColor = colorInput.value || BG_COLOR;
+    const ratio = ratioInput.value || RATIO;
     const aspect = image.width / image.height;
     const imgSize = size - padding * 2;
     let width = image.width || size;
@@ -54,11 +58,16 @@ function draw(image, size) {
         width = height * aspect;
     }
     console.log(width, height);
-    const x = (size - width) / 2;
-    const y = (size - height) / 2;
+    const isYokonaga = image.width > image.height;
+    const canvasWidth = isYokonaga ? size : size * ratio;
+    const canvasHeight = !isYokonaga ? size : size * ratio;
+    console.log(canvasWidth, canvasHeight);
+    setCanvasSize(canvasWidth, canvasHeight);
+    const x = (canvasWidth - width) / 2;
+    const y = (canvasHeight - height) / 2;
     ctx.save();
     ctx.fillStyle = bgColor;
-    ctx.fillRect(0, 0, size, size);
+    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
     ctx.beginPath();
     ctx.drawImage(image, x, y, width, height);
     ctx.restore();
